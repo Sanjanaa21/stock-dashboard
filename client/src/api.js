1937ae3c7007620466c5bbc8db1d2bc6892129ac
unknown <sanjanabhat622@gmail.com>
2025-12-18 09:35:44 +0530

    Fix API URL and CORS

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://stock-dashboard-2-aofp.onrender.com",
+  // baseURL: "https://stock-dashboard-2-aofp.onrender.com",
+  baseURL: "https://stock-dashboard-4.onrender.com";
 });
 
 export default API;
